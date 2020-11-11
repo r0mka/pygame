fe6d7997cdbb26546ee582171011819159905d03
r0mka <romanliveshere@gmail.com>
2020-11-11 04:11:24 -0800

    Add redraw_window function

--- main.py
+++ main.py
@@ -2,47 +2,55 @@ import pygame as pg
 
 # initialize pygame
 pg.init()
+run = True
 
 WIN_DIM = WIN_WIDTH, WIN_HEIGHT = 800, 600
 HERO_DIM = HERO_WIDTH, HERO_HEIGHT = 50, 100
 
-
 win = pg.display.set_mode(WIN_DIM)
-pygame.display.set_caption("First Game")
+pg.display.set_caption("First Game")
 
-run = True
+
 
 hero_pos = [0, 0]
 hero_vel = [5, 5]
 
-clock = pg.time.Clock()
 
-
-while run: 
-    clock.tick(60)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            run = false
-
-    keys = pg.key.get_pressed()
-    
-    
-    if keys[pg.K_LEFT] and hero_pos[0] >= hero_vel[0]:
-        hero_pos[0] -= hero_vel[0]
-    if keys[pg.K_RIGHT] and hero_pos[0] + hero_vel[0] <= WIN_WIDTH - HERO_WIDTH:
-        hero_pos[0] += hero_vel[0]
-    if keys[pg.K_UP] and hero_pos[1] >= hero_vel[1]:
-        hero_pos[1] -= hero_vel[1]
-    if keys[pg.K_DOWN]:
-        hero_pos[1] += hero_vel[1]
-
-
-    print(hero_pos)
-
-    
+def redraw_window():
     win.fill((0, 0, 0))
     pg.draw.rect(win, (255, 0, 0), (hero_pos[0], hero_pos[1], HERO_WIDTH, HERO_HEIGHT))
     pg.display.update()
+
+
+
+def main():
+    global run
+    clock = pg.time.Clock()
+    
+    while run: 
+        clock.tick(60)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = false
+
+        keys = pg.key.get_pressed()
+        
+        
+        if keys[pg.K_LEFT] and hero_pos[0] >= hero_vel[0]:
+            hero_pos[0] -= hero_vel[0]
+        if keys[pg.K_RIGHT] and hero_pos[0] + hero_vel[0] <= WIN_WIDTH - HERO_WIDTH:
+            hero_pos[0] += hero_vel[0]
+        if keys[pg.K_UP] and hero_pos[1] >= hero_vel[1]:
+            hero_pos[1] -= hero_vel[1]
+        if keys[pg.K_DOWN]:
+            hero_pos[1] += hero_vel[1]
+
+
+        redraw_window()
+
+    
+if __name__ == "__main__":
+    main()
 
 pg.quit()    
 
