00434dd360b0ed0c3a3e79601b2d7a85ed13f352
r0mka <romanliveshere@gmail.com>
2020-11-11 01:50:37 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame as pg
+
+# initialize pygame
+pg.init()
+
+WIN_DIM = WIN_WIDTH, WIN_HEIGHT = 800, 600
+HERO_DIM = HERO_WIDTH, HERO_HEIGHT = 50, 100
+
+
+win = pg.display.set_mode(WIN_DIM)
+
+run = True
+
+hero_pos = [0, 0]
+hero_vel = [5, 5]
+
+clock = pg.time.Clock()
+
+
+while run: 
+    clock.tick(60)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = false
+
+    keys = pg.key.get_pressed()
+    
+    
+    if keys[pg.K_LEFT] and hero_pos[0] >= hero_vel[0]:
+        hero_pos[0] -= hero_vel[0]
+    if keys[pg.K_RIGHT] and hero_pos[0] + hero_vel[0] <= WIN_WIDTH - HERO_WIDTH:
+        hero_pos[0] += hero_vel[0]
+    if keys[pg.K_UP] and hero_pos[1] >= hero_vel[1]:
+        hero_pos[1] -= hero_vel[1]
+    if keys[pg.K_DOWN]:
+        hero_pos[1] += hero_vel[1]
+
+
+    print(hero_pos)
+
+    
+    win.fill((0, 0, 0))
+    pg.draw.rect(win, (255, 0, 0), (hero_pos[0], hero_pos[1], HERO_WIDTH, HERO_HEIGHT))
+    pg.display.update()
+
+pg.quit()    
+
+
+
