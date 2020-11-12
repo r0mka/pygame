9acb9d8d35a7088ee1ece4282ba3d9e5fe1abd06
r0mka <romanliveshere@gmail.com>
2020-11-12 01:26:19 -0800

    Work on jump

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ HERO_DIM = HERO_WIDTH, HERO_HEIGHT = 50, 100
 win = pg.display.set_mode(WIN_DIM)
 pg.display.set_caption("First Game")
 
-
-
-hero_pos = [0, 0]
+hero_pos = [300, 400]
 hero_vel = [5, 5]
+
+jumpcount = 10
+isJump = False
 
 
 def redraw_window():
     win.fill((0, 0, 0))
     pg.draw.rect(win, (255, 0, 0), (hero_pos[0], hero_pos[1], HERO_WIDTH, HERO_HEIGHT))
+    pg.draw.line(win,(255, 0, 0), (0, 400 + HERO_HEIGHT), (WIN_WIDTH, 400 + HERO_HEIGHT))
     pg.display.update()
 
 
@@ -42,7 +44,17 @@ while run:
         hero_pos[1] -= hero_vel[1]
     if keys[pg.K_DOWN]:
         hero_pos[1] += hero_vel[1]
-
+    
+    if not isJump:
+        if keys[pg.K_SPACE]:
+            isJump = True
+    else:
+        if jumpcount >= -10:
+            hero_pos[1] -= jumpcount * abs(jumpcount) * 0.5
+            jumpcount -= 1
+        else:
+            isJump = False
+            jumpcount = 10
 
     redraw_window()
 
