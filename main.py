d5431b6b1b04b9606114181f2c82b965dea908dc
r0mka <romanliveshere@gmail.com>
2020-11-12 01:13:57 -0800

    remove main function

--- main.py
+++ main.py
@@ -22,36 +22,31 @@ def redraw_window():
     pg.display.update()
 
 
+   
+clock = pg.time.Clock()
 
-def main():
-    global run
-    clock = pg.time.Clock()
+while run: 
+    clock.tick(60)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            run = false
+
+    keys = pg.key.get_pressed()
     
-    while run: 
-        clock.tick(60)
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = false
-
-        keys = pg.key.get_pressed()
-        
-        
-        if keys[pg.K_LEFT] and hero_pos[0] >= hero_vel[0]:
-            hero_pos[0] -= hero_vel[0]
-        if keys[pg.K_RIGHT] and hero_pos[0] + hero_vel[0] <= WIN_WIDTH - HERO_WIDTH:
-            hero_pos[0] += hero_vel[0]
-        if keys[pg.K_UP] and hero_pos[1] >= hero_vel[1]:
-            hero_pos[1] -= hero_vel[1]
-        if keys[pg.K_DOWN]:
-            hero_pos[1] += hero_vel[1]
+    
+    if keys[pg.K_LEFT] and hero_pos[0] >= hero_vel[0]:
+        hero_pos[0] -= hero_vel[0]
+    if keys[pg.K_RIGHT] and hero_pos[0] + hero_vel[0] <= WIN_WIDTH - HERO_WIDTH:
+        hero_pos[0] += hero_vel[0]
+    if keys[pg.K_UP] and hero_pos[1] >= hero_vel[1]:
+        hero_pos[1] -= hero_vel[1]
+    if keys[pg.K_DOWN]:
+        hero_pos[1] += hero_vel[1]
 
 
-        redraw_window()
+    redraw_window()
 
     
-if __name__ == "__main__":
-    main()
-
 pg.quit()    
 
 
